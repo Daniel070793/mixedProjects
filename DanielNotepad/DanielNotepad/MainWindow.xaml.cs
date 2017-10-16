1d14c96e1ed90575dd383e418302b3485f2af4b1
Daniel Sørensen <1058493@ucn.dk>
2017-10-16 16:49:35 +0200

    added feature to save file and create new file

--- DanielNotepad/DanielNotepad/MainWindow.xaml.cs
+++ DanielNotepad/DanielNotepad/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,61 @@ namespace DanielNotepad
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void New_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void New_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if(txtEditor.Text == "")
+            {
+                txtEditor.Text = "";
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Do you want to save changes?", "Danielpad", MessageBoxButton.YesNoCancel);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        save();
+                        break;
+                    case MessageBoxResult.No:
+                        txtEditor.Text = "";
+                        break;
+                    case MessageBoxResult.Cancel:
+                        MessageBox.Show("fiks så den går tilbage til editor");
+                        break;                      
+                }
+            }
+            
+        } 
+
+        private void save()
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+
+            saveFile.Title = "save file";
+            saveFile.Filter = "Text Files (*.txt)|*.txt| All Files (*.*)|*.*";
+            if (saveFile.ShowDialog() == true)
+            {
+                StreamWriter write = new StreamWriter(File.Create(saveFile.FileName));
+
+                write.Write(txtEditor.Text);
+                write.Dispose();
+            }
+        }
+
+        private void Save_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            save();
         }
     }
 }
