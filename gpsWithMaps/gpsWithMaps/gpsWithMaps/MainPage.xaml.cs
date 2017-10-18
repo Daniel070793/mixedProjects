fb45257b3f9cc5b94bc21aa30f8afb901045b00a
Daniel Sørensen <1058493@ucn.dk>
2017-10-18 22:08:44 +0200

    added documentation reference

--- gpsWithMaps/gpsWithMaps/gpsWithMaps/MainPage.xaml.cs
+++ gpsWithMaps/gpsWithMaps/gpsWithMaps/MainPage.xaml.cs
@@ -15,7 +15,7 @@ namespace gpsWithMaps
             InitializeComponent();
 
 
-
+            //For at oprette api_key til project: https://developer.xamarin.com/guides/android/platform_features/maps_and_location/maps/obtaining_a_google_maps_api_key/ 
             var map = new Map(MapSpan.FromCenterAndRadius(
                 new Position(57.048820, 9.921747),
                 Distance.FromMeters(0.5)))
