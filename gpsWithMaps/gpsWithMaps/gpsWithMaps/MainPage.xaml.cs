42d227cf1a58c66070903577f7a33e6bbdd0385d
Daniel Sørensen <1058493@ucn.dk>
2017-10-18 21:55:36 +0200

    added maps

---
+++ gpsWithMaps/gpsWithMaps/gpsWithMaps/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace gpsWithMaps
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+            var map = new Map(MapSpan.FromCenterAndRadius(
+                new Position(57.048820, 9.921747),
+                Distance.FromMeters(0.5)))
+            {
+                IsShowingUser = true,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+        }
+    }
+}
